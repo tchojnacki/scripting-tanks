1491660ad005715b02423eebed786dfce9b88a74
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-10-11 21:03:33 +0200

    Add tests for RgbToString

--- Backend.Tests/ColorUtilsTest.cs
+++ Backend.Tests/ColorUtilsTest.cs
@@ -31,4 +31,28 @@ public class ColorUtilsTest
         Action act = () => ColorUtils.HslToRgb(hue, saturation, lightness);
         act.Should().Throw<ArgumentOutOfRangeException>();
     }
+
+    [Theory]
+    [InlineData(0, 0, 0, "#000000")]
+    [InlineData(1, 1, 1, "#ffffff")]
+    [InlineData(0, 1, 1, "#00ffff")]
+    [InlineData(128.0 / 255, 128.0 / 255, 128.0 / 255, "#808080")]
+    public void RgbToString_ShouldStringify_WhenGivenCorrectRgb(double red, double green, double blue, string expected)
+    {
+        var result = ColorUtils.RgbToString(red, green, blue);
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(-1, 0, 0)]
+    [InlineData(1.5, 0, 0)]
+    [InlineData(0, -0.01, 0)]
+    [InlineData(0, 2, 0)]
+    [InlineData(0, 0, -0.5)]
+    [InlineData(0, 0, 3.14)]
+    public void RgbToString_ShouldThrow_WhenGivenInvalidRgbValues(double red, double green, double blue)
+    {
+        Action act = () => ColorUtils.RgbToString(red, green, blue);
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
 }
