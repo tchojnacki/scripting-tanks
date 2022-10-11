b80af0a803ce05435c6720cd4cf9583ca02f5cad
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-10-11 21:16:41 +0200

    Add test for MathUtils

---
+++ Backend.Tests/MathUtilsTests.cs
@@ -0,0 +1,32 @@
+using Backend.Utils.Common;
+
+using static System.Math;
+
+namespace Backend.Tests;
+
+public class MathUtilsTests
+{
+    [Theory]
+    [InlineData(1.5, 1.5, 0)]
+    [InlineData(0, PI / 4, PI / 4)]
+    [InlineData(PI / 4, -PI / 4, -PI / 2)]
+    [InlineData(PI / 2, 13 * PI / 2, 0)]
+    [InlineData(0, -PI, -PI)]
+    public void AngleDiff_ShouldReturnSignedDiff_WhenGivenTwoAngles(double from, double to, double expected)
+    {
+        var result = MathUtils.AngleDiff(from, to);
+        result.Should().BeApproximately(expected, 0.01);
+    }
+
+    [Theory]
+    [InlineData(1.5, 1.5, 0)]
+    [InlineData(0, PI / 4, PI / 4)]
+    [InlineData(PI / 4, -PI / 4, PI / 2)]
+    [InlineData(PI / 2, 13 * PI / 2, 0)]
+    [InlineData(0, -PI, PI)]
+    public void AbsAngleDiff_ShouldReturnUnsignedDiff_WhenGivenTwoAngles(double alpha, double beta, double expected)
+    {
+        var result = MathUtils.AbsAngleDiff(alpha, beta);
+        result.Should().BeApproximately(expected, 0.01);
+    }
+}
